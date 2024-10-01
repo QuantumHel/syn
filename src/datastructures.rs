de6757a7a9d01bd550bdb83c365e9818699026a8
Keefe Huang <keefe.huang@tum.de>
2024-10-01 16:18:40 +0200

    Shift function order to be more readable

--- src/datastructures.rs
+++ src/datastructures.rs
@@ -11,17 +11,6 @@ where
     fn cx(&mut self, control: IndexType, target: IndexType) -> &mut Self;
     fn s(&mut self, target: IndexType) -> &mut Self;
     fn v(&mut self, target: IndexType) -> &mut Self;
-    fn y(&mut self, target: IndexType) -> &mut Self {
-        self.s_dgr(target).x(target).s(target)
-    }
-
-    fn x(&mut self, target: IndexType) -> &mut Self {
-        self.v(target).v(target)
-    }
-
-    fn z(&mut self, target: IndexType) -> &mut Self {
-        self.s(target).s(target)
-    }
 
     fn s_dgr(&mut self, target: IndexType) -> &mut Self {
         self.z(target).s(target)
@@ -29,6 +18,18 @@ where
 
     fn v_dgr(&mut self, target: IndexType) -> &mut Self {
         self.x(target).v(target)
+    }
+
+    fn x(&mut self, target: IndexType) -> &mut Self {
+        self.v(target).v(target)
+    }
+
+    fn y(&mut self, target: IndexType) -> &mut Self {
+        self.s_dgr(target).x(target).s(target)
+    }
+
+    fn z(&mut self, target: IndexType) -> &mut Self {
+        self.s(target).s(target)
     }
 
     fn h(&mut self, target: IndexType) -> &mut Self {
