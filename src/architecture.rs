ab174d967fa4f9da25f6ab339544381f2094259f
Keefe Huang <keefe.huang@tum.de>
2024-10-30 15:13:25 +0100

    Add Connectivity trait

--- src/architecture.rs
+++ src/architecture.rs
@@ -1,1 +1,9 @@
-mod connectivity;
+pub mod complete;
+pub mod line;
+
+pub trait Architecture {
+    fn best_path(&self, i: usize, j: usize) -> Vec<usize>;
+    fn distance(&self, i: usize, j: usize) -> usize;
+    fn neighbors(&self, i: usize) -> Vec<usize>;
+    fn non_cutting(&mut self) -> &Vec<usize>;
+}
