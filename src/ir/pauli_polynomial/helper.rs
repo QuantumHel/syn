ca1832942f34f41b6c538be0112e54db41c7317e
Keefe Huang <keefe.huang@tum.de>
2025-04-08 09:44:53 +0200

    Update naive pp synth to pushdown, upated helper functions for pp synth, corrected MaskedPropagateClifford for pp
    - Ensure that only the masked PP is masked. Everything else applies in full
    - Implemented PropagateClifford for VecDequeu<PauliPolynomial>
    - Removed doubled variable
    - Shifted impl for PP into helper.rs

--- src/ir/pauli_polynomial/helper.rs
+++ src/ir/pauli_polynomial/helper.rs
@@ -1,5 +1,6 @@
 use std::collections::VecDeque;
 
+use bitvec::{bitvec, order::Lsb0};
 use itertools::Itertools;
 
 use crate::{
@@ -8,14 +9,68 @@ use crate::{
     },
     ir::{CliffordGates, Gates},
 };
+use bitvec::prelude::BitVec;
+
+impl PropagateClifford for VecDeque<PauliPolynomial> {
+    fn cx(&mut self, control: usize, target: usize) -> &mut Self {
+        for pauli_polynomial in self.iter_mut() {
+            pauli_polynomial.cx(control, target);
+        }
+        self
+    }
+
+    fn s(&mut self, target: usize) -> &mut Self {
+        for pauli_polynomial in self.iter_mut() {
+            pauli_polynomial.s(target);
+        }
+        self
+    }
+
+    fn v(&mut self, target: usize) -> &mut Self {
+        for pauli_polynomial in self.iter_mut() {
+            pauli_polynomial.v(target);
+        }
+        self
+    }
+}
+
+impl MaskedPropagateClifford for VecDeque<PauliPolynomial> {
+    fn masked_cx(&self, control: usize, target: usize, mask: &BitVec) -> &Self {
+        self[0].masked_cx(control, target, mask);
+        for pauli_polynomial in self.iter().skip(1) {
+            pauli_polynomial.masked_cx(
+                control,
+                target,
+                &bitvec![usize, Lsb0; 1; pauli_polynomial.length()],
+            );
+        }
+        self
+    }
+
+    fn masked_s(&self, target: usize, mask: &BitVec) -> &Self {
+        self[0].masked_s(target, mask);
+        for pauli_polynomial in self.iter().skip(1) {
+            pauli_polynomial.masked_s(target, &bitvec![usize, Lsb0; 1; pauli_polynomial.length()]);
+        }
+        self
+    }
+
+    fn masked_v(&self, target: usize, mask: &BitVec) -> &Self {
+        self[0].masked_v(target, mask);
+        for pauli_polynomial in self.iter().skip(1) {
+            pauli_polynomial.masked_v(target, &bitvec![usize, Lsb0; 1; pauli_polynomial.length()]);
+        }
+        self
+    }
+}
 
 pub(super) fn push_down_pauli_polynomial_update<G>(
-    pauli_polynomials: &VecDeque<PauliPolynomial>,
+    pauli_polynomials: &mut VecDeque<PauliPolynomial>,
     repr: &mut G,
     clifford_tableau: &mut CliffordTableau,
     pauli_polynomial: PauliPolynomial,
     num_gadgets: usize,
-    mut mask: bitvec::prelude::BitVec,
+    mut mask: BitVec,
 ) where
     G: CliffordGates + Gates,
 {
@@ -27,14 +82,14 @@ pub(super) fn push_down_pauli_polynomial_update<G>(
                 PauliLetter::X => {
                     affected_qubits.push(i);
                     pauli_polynomial.masked_h(i, &mask);
-                    pauli_polynomials.masked_h(i, &mask);
+                    pauli_polynomials.h(i);
                     clifford_tableau.h(i);
                     repr.h(i);
                 }
                 PauliLetter::Y => {
                     affected_qubits.push(i);
                     pauli_polynomial.masked_s(i, &mask);
-                    pauli_polynomials.masked_s(i, &mask);
+                    pauli_polynomials.s(i);
                     clifford_tableau.s(i);
                     repr.s(i);
                 }
--- src/ir/pauli_polynomial/naive.rs
+++ src/ir/pauli_polynomial/naive.rs
@@ -1,12 +1,12 @@
 use std::collections::VecDeque;
 
 use crate::{
-    data_structures::{CliffordTableau, MaskedPropagateClifford, PauliPolynomial},
+    data_structures::{CliffordTableau, PauliPolynomial},
     ir::{CliffordGates, Gates, Synthesizer},
 };
 use bitvec::{bitvec, order::Lsb0};
 
-use super::helper::naive_pauli_polynomial_update;
+use super::helper::push_down_pauli_polynomial_update;
 
 #[derive(Default)]
 pub struct NaivePauliPolynomialSynthesizer {
@@ -16,28 +16,6 @@ pub struct NaivePauliPolynomialSynthesizer {
 impl NaivePauliPolynomialSynthesizer {
     pub fn set_clifford_tableau(&mut self, clifford_tableau: CliffordTableau) -> &mut Self {
         self.clifford_tableau = clifford_tableau;
-        self
-    }
-}
-
-impl MaskedPropagateClifford for VecDeque<PauliPolynomial> {
-    fn masked_cx(
-        &self,
-        control: crate::IndexType,
-        target: crate::IndexType,
-        mask: &bitvec::prelude::BitVec,
-    ) -> &Self {
-        let _ = self.iter().map(|pp| pp.masked_cx(control, target, mask));
-        self
-    }
-
-    fn masked_s(&self, target: crate::IndexType, mask: &bitvec::prelude::BitVec) -> &Self {
-        let _ = self.iter().map(|pp| pp.masked_s(target, mask));
-        self
-    }
-
-    fn masked_v(&self, target: crate::IndexType, mask: &bitvec::prelude::BitVec) -> &Self {
-        let _ = self.iter().map(|pp| pp.masked_v(target, mask));
         self
     }
 }
@@ -55,11 +33,10 @@ where
         let mut clifford_tableau = std::mem::take(&mut self.clifford_tableau);
         while !pauli_polynomials.is_empty() {
             let pauli_polynomial = pauli_polynomials.pop_front().unwrap();
-            let num_gadgets = pauli_polynomial.length();
-            let gadget_length = pauli_polynomial.length();
-            let mask = bitvec![usize, Lsb0; 1; gadget_length];
+            let num_gadgets: usize = pauli_polynomial.length();
+            let mask = bitvec![usize, Lsb0; 1; num_gadgets];
             push_down_pauli_polynomial_update(
-                &pauli_polynomials,
+                &mut pauli_polynomials,
                 repr,
                 &mut clifford_tableau,
                 pauli_polynomial,
