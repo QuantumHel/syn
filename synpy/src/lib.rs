20e1549be435d9d7b9e5b6b878d8120b33d0f211
Richie Yeung <richie.yeung1@gmail.com>
2025-04-12 01:21:44 +0100

    feat: implement PyCliffordTableau

--- synpy/src/lib.rs
+++ synpy/src/lib.rs
@@ -1,8 +1,10 @@
 mod synthesis;
+mod tableau;
 mod validation;
 
 use crate::synthesis::synthesize_pauli_exponential;
 use crate::synthesis::{PyCommand, PyPauliString};
+use crate::tableau::PyCliffordTableau;
 use pyo3::prelude::{PyModule, PyModuleMethods};
 use pyo3::{pymodule, wrap_pyfunction, Bound, FromPyObject, PyRef, PyResult, Python};
 use std::ops::Deref;
@@ -14,6 +16,7 @@ fn synpy_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<PyPauliString>()?;
     m.add_class::<PyCommand>()?;
     let _ = m.add_function(wrap_pyfunction!(synthesize_pauli_exponential, m)?);
+    m.add_class::<PyCliffordTableau>()?;
 
     Ok(())
 }
