218d9d8d7d45e5f09986b0cc62daa9a1ee0dc643
Keefe Huang <keefe.huang@tum.de>
2025-08-06 17:43:38 +0200

    Add tests for PSGS implementation, extend synthesis method enum to include PSGS

--- synir/tests/pauli_polynomial.rs
+++ synir/tests/pauli_polynomial.rs
@@ -88,3 +88,55 @@ fn test_naive_pauli_exponential_synthesis_complex() {
     assert_eq!(mock.commands(), &ref_commands);
     assert_eq!(ct, parse_clifford_commands(4, &ref_clifford_commands));
 }
+
+#[test]
+fn test_psgs_pauli_exponential_synthesis_simple() {
+    let pp = setup_simple_pp();
+    let mut mock = MockCircuit::new();
+    let mut synthesizer = PSGSPauliPolynomialSynthesizer::default();
+    synthesizer.set_clifford_tableau(CliffordTableau::new(4));
+    synthesizer.set_connectivity(Connectivity::complete(4));
+    let ct = synthesizer.synthesize(pp, &mut mock);
+
+    let ref_commands = 
+        [MockCommand::S(1), MockCommand::CX(3, 1), MockCommand::V(2), MockCommand::CX(2, 1), MockCommand::Ry(1, 0.3)];
+
+    let ref_clifford_commands = [
+        MockCommand::S(1), MockCommand::CX(3, 1), MockCommand::V(2), MockCommand::CX(2, 1)
+    ];
+
+    assert_eq!(mock.commands(), &ref_commands);
+    assert_eq!(ct, parse_clifford_commands(4, &ref_clifford_commands));
+}
+
+
+#[test]
+fn test_psgs_pauli_exponential_synthesis_complex() {
+    let pp = setup_complex_pp();
+    let mut mock = MockCircuit::new();
+    let mut synthesizer = PSGSPauliPolynomialSynthesizer::default();
+    synthesizer.set_clifford_tableau(CliffordTableau::new(4));
+    synthesizer.set_connectivity(Connectivity::complete(4));
+    let ct = synthesizer.synthesize(pp, &mut mock);
+
+    let ref_commands = [
+        MockCommand::CX(3, 1), 
+        MockCommand::CX(2, 1), 
+        MockCommand::Rz(1, 0.3), 
+        MockCommand::H(1), 
+        MockCommand::S(0), 
+        MockCommand::CX(1, 0), 
+        MockCommand::Ry(0, 0.7)
+        ];
+
+    let ref_clifford_commands = [
+        MockCommand::CX(3, 1), 
+        MockCommand::CX(2, 1), 
+        MockCommand::H(1), 
+        MockCommand::S(0), 
+        MockCommand::CX(1, 0), 
+    ];
+    
+    assert_eq!(mock.commands(), &ref_commands);
+    assert_eq!(ct, parse_clifford_commands(4, &ref_clifford_commands));
+}
