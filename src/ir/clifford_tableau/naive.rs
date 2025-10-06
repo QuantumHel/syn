743c870dac73287b0aa54829e660a574106d2afd
Keefe Huang <keefe.huang@tum.de>
2025-10-06 17:06:56 +0200

    Replace clean_naive_pivot with clean_pivot

--- src/ir/clifford_tableau/naive.rs
+++ src/ir/clifford_tableau/naive.rs
@@ -4,9 +4,11 @@ use crate::{
 };
 
 use super::helper::{
-    clean_naive_pivot, clean_signs, clean_x_observables, clean_z_observables, naive_pivot_search,
+    clean_pivot, clean_naive_pivot, clean_signs, clean_x_observables, clean_z_observables, naive_pivot_search,
     swap,
 };
+
+use crate::data_structures::PauliLetter;
 
 #[derive(Default)]
 pub struct NaiveCliffordSynthesizer {}
@@ -26,13 +28,16 @@ where
             }
 
             // Cleanup pivot column
-            clean_naive_pivot(repr, &mut clifford_tableau, row, row);
+            // clean_naive_pivot(repr, &mut clifford_tableau, row, row);
+            clean_pivot(repr, &mut clifford_tableau, row, row, PauliLetter::X);
 
             let checked_rows = (row + 1..num_qubits).collect::<Vec<_>>();
 
             // Use the pivot to remove all other terms in the X observable.
             clean_x_observables(repr, &mut clifford_tableau, &checked_rows, row, row);
 
+            clean_pivot(repr, &mut clifford_tableau, row, row, PauliLetter::Z);
+            
             // Use the pivot to remove all other terms in the Z observable.
             clean_z_observables(repr, &mut clifford_tableau, &checked_rows, row, row);
         }
