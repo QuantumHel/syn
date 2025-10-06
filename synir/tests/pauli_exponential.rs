d8cc29022f8d2433a047af336c642ba7eb00ded8
Keefe Huang <keefe.huang@tum.de>
2025-10-06 17:06:56 +0200

    Replace clean_naive_pivot with clean_pivot

--- synir/tests/pauli_exponential.rs
+++ synir/tests/pauli_exponential.rs
@@ -3,12 +3,12 @@ mod common;
 use std::collections::VecDeque;
 
 use common::{parse_clifford_commands, MockCircuit, MockCommand};
-use synir::data_structures::{CliffordTableau, PauliPolynomial};
-use synir::ir::clifford_tableau::{CliffordTableauSynthStrategy, NaiveCliffordSynthesizer};
-use synir::ir::pauli_exponential::PauliExponential;
-use synir::ir::pauli_exponential::PauliExponentialSynthesizer;
-use synir::ir::pauli_polynomial::PauliPolynomialSynthStrategy;
-use synir::ir::Synthesizer;
+use syn::data_structures::{CliffordTableau, HasAdjoint, PauliPolynomial};
+use syn::ir::clifford_tableau::{CliffordTableauSynthStrategy, NaiveCliffordSynthesizer};
+use syn::ir::pauli_exponential::PauliExponential;
+use syn::ir::pauli_exponential::PauliExponentialSynthesizer;
+use syn::ir::pauli_polynomial::PauliPolynomialSynthStrategy;
+use syn::ir::Synthesizer;
 
 fn setup_simple_pe() -> PauliExponential {
     let ham = vec![("IZZZ", 0.3)];
@@ -79,29 +79,28 @@ fn test_naive_pauli_exponential_complex() {
 
     let mut cliff_synthesizer = NaiveCliffordSynthesizer::default();
 
-    cliff_synthesizer.synthesize(ref_ct.clone(), &mut mock_ct);
+    cliff_synthesizer.synthesize(ref_ct.clone().adjoint(), &mut mock_ct);
 
     let mock_ct_ref_commands = [
-        MockCommand::H(1),
         MockCommand::CX(0, 1),
         MockCommand::CX(0, 2),
+        MockCommand::CX(0, 3),
         MockCommand::H(1),
         MockCommand::H(2),
+        MockCommand::H(3),
         MockCommand::CX(1, 0),
         MockCommand::CX(2, 0),
-        MockCommand::H(1),
-        MockCommand::S(1),
-        MockCommand::CX(1, 2),
+        MockCommand::CX(3, 0),
+        MockCommand::V(1),
+        MockCommand::V(3),
+        MockCommand::CX(2, 1),
+        MockCommand::CX(3, 1),
         MockCommand::CX(3, 2),
         MockCommand::CX(2, 3),
         MockCommand::CX(3, 2),
-        MockCommand::H(2),
         MockCommand::CX(3, 2),
-        MockCommand::S(3),
         MockCommand::X(1),
         MockCommand::X(2),
-        MockCommand::X(3),
-        MockCommand::Z(1),
     ];
 
     assert_eq!(mock_ct.commands(), &mock_ct_ref_commands);
@@ -131,9 +130,7 @@ fn test_naive_pauli_exponential_complex() {
         MockCommand::CX(1, 0),
         MockCommand::CX(2, 0),
         MockCommand::CX(3, 0),
-        MockCommand::S(1),
-        MockCommand::H(1),
-        MockCommand::S(1),
+        MockCommand::V(1),
         MockCommand::V(3),
         MockCommand::CX(2, 1),
         MockCommand::CX(3, 1),
@@ -141,6 +138,7 @@ fn test_naive_pauli_exponential_complex() {
         MockCommand::CX(2, 3),
         MockCommand::CX(3, 2),
         MockCommand::CX(3, 2),
+        MockCommand::X(1),
         MockCommand::X(2),
     ];
 
