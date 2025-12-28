f264893a995a029e1e5bfe0554e9e0b61b11a1a7
Keefe Huang <keefe.huang@tum.de>
2025-12-28 14:36:34 +0100

    Implement PropagateClifford for PauliExponential

--- synir/src/data_structures/pauli_exponential.rs
+++ synir/src/data_structures/pauli_exponential.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use crate::data_structures::{CliffordTableau, PauliPolynomial};
+use crate::data_structures::{CliffordTableau, PauliPolynomial, PropagateClifford};
 
 #[derive(Default)]
 pub struct PauliExponential {
@@ -17,5 +17,33 @@ impl PauliExponential {
             pauli_polynomials,
             clifford_tableau,
         }
+    }
+
+    pub fn chains(&mut self) -> &mut VecDeque<PauliPolynomial>{
+        &mut self.pauli_polynomials
+    }
+
+    pub fn size(&self) -> usize{
+        self.clifford_tableau.size()
+    }
+}
+
+impl PropagateClifford for PauliExponential{
+    fn cx(&mut self, control: crate::IndexType, target: crate::IndexType) -> &mut Self {
+        self.pauli_polynomials.cx(control, target);
+        self.clifford_tableau.cx(control, target);
+        self
+    }
+
+    fn s(&mut self, target: crate::IndexType) -> &mut Self {
+        self.pauli_polynomials.s(target);
+        self.clifford_tableau.s(target);
+        self
+    }
+
+    fn v(&mut self, target: crate::IndexType) -> &mut Self {
+        self.pauli_polynomials.v(target);
+        self.clifford_tableau.v(target);
+        self
     }
 }
